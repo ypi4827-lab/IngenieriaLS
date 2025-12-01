bd49725b187c7bed7f6098350f7eb9e84d574fdd
edgar villafañe ortiz <ypi4827@gmail.com>
2025-12-01 18:37:47 -0500

    arreglar error en card de confirmacion al mostrar los datos

--- frontend/src/componentes/confirmaciones/CardConfirmacion.tsx
+++ frontend/src/componentes/confirmaciones/CardConfirmacion.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import "../../estilos/confirmaciones.css";
+import React, { useEffect, useState } from 'react';
+import '../../estilos/confirmaciones.css';
+import { obtenerUsuarios } from '../../servicios/usuarios';
 
 interface Reserva {
   servicio: string;
-  tecnico: string;
+  tecnicoAsignado: string;
   fechaProgramada: string;
-  hora: string;
+  horaProgramada: string;
   estado: string;
 }
 
@@ -14,19 +15,36 @@ interface Props {
 }
 
 const CardConfirmacion: React.FC<Props> = ({ reserva }) => {
-  const { servicio, tecnico, fechaProgramada, hora, estado } = reserva;
+  const { servicio, tecnicoAsignado, fechaProgramada, horaProgramada, estado } = reserva;
+  const [tecnico, setTecnico] = useState('');
+  useEffect(() => {
+    const nombreTecnico = async () => {
+      const usuarios = await obtenerUsuarios();
+      const tecnico = usuarios.filter((u) => u._id === tecnicoAsignado);
+      setTecnico(tecnico[0].nombre);
+    };
+    nombreTecnico();
+  }, []);
 
   return (
     <div className="card-confirmacion">
       <div className="icono-exito">✅</div>
       <h3>{estado}</h3>
-      <p><strong>Servicio:</strong> {servicio}</p>
-      <p><strong>Técnico asignado:</strong> {tecnico}</p>
-      <p><strong>Fecha:</strong> {fechaProgramada}</p>
-      <p><strong>Hora:</strong> {hora}</p>
+      <p>
+        <strong>Servicio:</strong> {servicio}
+      </p>
+      <p>
+        <strong>Técnico asignado:</strong> {tecnico}
+      </p>
+      <p>
+        <strong>Fecha:</strong> {fechaProgramada}
+      </p>
+      <p>
+        <strong>Hora:</strong> {horaProgramada}
+      </p>
       <p className="mensaje-final">
-        Te enviaremos un correo con los detalles de la cita.  
-        ¡Gracias por confiar en Ingeniería LS!
+        Te enviaremos un correo con los detalles de la cita. ¡Gracias por
+        confiar en Ingeniería LS!
       </p>
     </div>
   );
